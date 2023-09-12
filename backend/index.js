e8e427daa6e790cfbdf7b178ba59953818e82c74
Adiguna <adigunarizqi@gmail.com>
2023-09-12 22:33:41 +0700

    Login di controller/User.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+dotenv.config();
 // import Users from "./models/UserModel.js";
 const app = express();
 
