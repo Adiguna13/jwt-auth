13e5e5b7b3071f80488529b3332e5de81f99eee8
Adiguna <adigunarizqi@gmail.com>
2023-09-12 21:36:20 +0700

    controller/Users and routes/index

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import db from "./config/Database.js";
+import router from "./routes/index.js";
 // import Users from "./models/UserModel.js";
 const app = express();
 
 try {
   await db.authenticate();
   console.log("Database Connected...");
-//   await Users.sync();
+  //   await Users.sync();
 } catch (error) {
   console.log(error);
 }
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log("Server running at port 5000"));
